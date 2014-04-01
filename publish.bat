3e5491830e60e76e1a18c32be339da3661cd574d
Nuno Brito <nuno.brito@esa.int>
2014-04-01 23:32:19 +0200

    removed java binaries from this repository

--- publish.bat
+++ publish.bat
@@ -1,27 +1,45 @@
 @echo off
+
+set zipfile=triplecheck-0.0.zip
+
+echo building the new executable
+"c:\Program Files (x86)\Launch4j\launch4jc.exe" .\docs\launch4j-triplecheck.xml
+
+
 echo deleting old plugin folder
-for /f "tokens=* delims=" %%i in ('dir ..\publish\plugins /s/b') do (
+cd ..\publish
+for /f "tokens=* delims=" %%i in ('dir plugins /s/b') do (
   rd /s /q "%%i"
 )
 
 
-
 echo copying the new plugin files
+cd ..\reporter
 xcopy /s/y run\plugins ..\publish\plugins
 
 
 echo Creating a new zip file
+del ..\%zipfile% 
+
+::pause
+
 cd ..\publish
-..\reporter\tools\7z.exe a -r ..\test.zip 
+..\reporter\tools\7z.exe a -r ..\%zipfile% 
 
 
 echo deleting the test folder
 cd..
 rd /s /q test
 
+
 echo extracting files for testing there
 mkdir test
-..\reporter\tools\7z.exe x 
+cd test
+..\reporter\tools\7z.exe x ..\%zipfile%
 
 
-pause
+echo opening explorer
+explorer .
+
+echo launching triplecheck
+triplecheck.exe
