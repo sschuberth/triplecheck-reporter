54b94985661a515b3fac6f083eaa678b7da18498
Nuno Brito <nuno.brito@esa.int>
2014-04-15 17:57:09 +0200

    Complete source for version 0.6

---
+++ run/triggers/MIT.java
@@ -0,0 +1,100 @@
+
+import definitions.TriggerType;
+import java.io.File;
+import java.util.Date;
+import script.Trigger;
+
+/*
+ * SPDXVersion: SPDX-1.1
+ * Creator: Person: Nuno Brito (nuno.brito@triplecheck.de)
+ * Creator: Organization: TripleCheck (contact@triplecheck.de)
+ * Created: 2014-04-06T00:00:00Z
+ * LicenseName: EUPL-1.1-without-appendix
+ * FileName: MIT.java  
+ * FileType: SOURCE
+ * FileCopyrightText: <text> Copyright 2014 Nuno Brito, TripleCheck </text>
+ * FileComment: <text> Given a text file, try to identify portions of text
+ *  that allows us to distinguish if the file is covered under a specific 
+ *  license and which version is applicable when possible.</text> 
+ */
+
+
+/**
+ *
+ * @author Nuno Brito, 6th of April 2014 in Darmstadt, Germany.
+ *  nuno.brito@triplecheck.de | http://nunobrito.eu
+ */
+public class MIT implements Trigger {
+    
+    // the list of id's that we can use to identify a license
+    String[] list = {
+        "under MIT license",
+        "Permission is hereby granted, free of charge, to any person obtaining"
+    };
+    
+    /**
+     * Verifies if the provided text applies to the triggers that
+     * included on this license.
+     * @param text Text to be analysed
+     * @return 
+     */
+    @Override
+    public Boolean isApplicable(String text){
+        // iterate all our ids
+        for(String id : list){
+            if(text.contains(id)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public Boolean isApplicable(File file) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public String getShortIdentifier() {
+        return "MIT";
+    }
+
+    @Override
+    public String getURL() {
+        return "http://spdx.org/licenses/MIT";
+    }
+
+    @Override
+    public Boolean supportsBinaries() {
+        return false;
+    }
+
+    @Override
+    public Boolean supportsTextFiles() {
+        return true;
+    }
+
+   
+    @Override
+    public Date getDatePublished() {
+        return utils.time.getDate(2000, 01, 01);
+    }
+    
+    @Override
+    public TriggerType getType(){
+        return TriggerType.LICENSE;
+    }
+
+    
+
+    @Override
+    public String getFullName() {
+        return "MIT License";
+    }
+
+    @Override
+    public String getResult() {
+        return LicenseInfoInFile + getShortIdentifier();
+    }
+    
+}
