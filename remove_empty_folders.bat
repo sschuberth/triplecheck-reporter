874d88addcb4c45b22835757fef768b6c523747c
Nuno Brito <nuno.brito@esa.int>
2014-05-09 17:46:28 +0200

    - clean up

--- remove_empty_folders.bat
+++
@@ -1,7 +0,0 @@
-:: git doesn't like empty folders so we need to delete them
-rmdir build\generated-sources\ap-source-output
-rmdir build\generated-sources
-rmdir build\empty
-rmdir dist\lib
-rmdir dist
-pause
