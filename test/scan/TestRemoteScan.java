6dd17ec304926a8d473a88f324ce113fa25ba97f
Nuno Brito <nuno.brito@triplecheck.de>
2015-02-23 15:26:49 +0100

    Adding prototype for testing the remote scan features

---
+++ test/scan/TestRemoteScan.java
@@ -0,0 +1,56 @@
+/**
+ * SPDXVersion: SPDX-1.1
+ * Creator: Person: Nuno Brito (nuno.brito@triplecheck.de)
+ * Creator: Organization: TripleCheck (http://triplecheck.net)
+ * Created: 2015-02-23T13:25:01Z
+ * LicenseName: EUPL-1.1-without-appendix
+ * FileCopyrightText: <text> Copyright (c) 2015 Nuno Brito, TripleCheck </text>
+ * FileComment: <text> Test a remote scan </text> 
+ */
+package scan;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Nuno Brito, 23rd of February 2015 in Frankfurt, Germany
+ */
+public class TestRemoteScan {
+    
+    public TestRemoteScan() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void hello() {
+        /*
+        For this test we are assuming that you already have an exchange server
+        running somewhere to provide the analysis result. These test cases are
+        only intended for testing the client-isde of the analysis. If something
+        is not clear, please to get in contact @nn81.
+        */
+    
+        // see ExchangeTest.java for an actual test being performed
+    
+    }
+}
