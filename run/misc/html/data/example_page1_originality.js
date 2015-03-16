98ff34a9702a348f0ed8a5426586fe2410a4c5c3
Nuno Brito <nuno.brito@triplecheck.de>
2015-03-16 18:35:46 +0100

    Adding example for page1_originality.js

---
+++ run/misc/html/data/example_page1_originality.js
@@ -0,0 +1,8 @@
+/*
+ * Graph sorting the files according to:
+ *  - Original (authored)
+ *  - External (authored by a third-party project or developer)
+ *  - Auto-generated (not relevant from a copyright/license perspective)
+ *  - Mixed code (Partly authored by a third-party, partly by this author)
+ */
+var fileOriginality = [47.27, 65.32, 84.59, 11.86];
